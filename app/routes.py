b742424286ac412b67b8486e680bc8c1217a3abc
jmt75200 <joelle@pcgamerzhawaii.com>
2018-03-17 16:55:21 -1000

    adding registration page

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,7 @@ from app import app
 def index():
   user = {'username' : 'Ginga', 'password' : '12345safe'}
   return render_template('index.html', title="whatever", user=user)
+
+@app.route('/register')
+def register():
+  return render_template('register.html', msg="do et now")
